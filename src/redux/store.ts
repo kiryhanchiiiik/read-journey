154813c9b42d53155e6a20080bc6d7d6e5775042
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-06-27 13:23:58 +0300

    add persistor

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/slice";
 import { useDispatch } from "react-redux";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const authConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistReducer(authConfig, authReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const persistor = persistStore(store);

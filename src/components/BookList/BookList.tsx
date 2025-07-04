86b75e479f97f0f9c7e212b19b1f0e002f6e4290
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-07-04 13:03:14 +0300

    add fetch books

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -3,16 +3,21 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { authInstance } from "../../redux/auth/operations";
+
 const BookList = () => {
-  const [books, setBooks] = useState(null);
+  const [books, setBooks] = useState<any[] | null>(null);
+
+  // refs для кастомных кнопок
+  const prevRef = useRef<HTMLButtonElement>(null);
+  const nextRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const fetchBooks = async () => {
       try {
         const response = await authInstance.get("/books/recommend");
-        setBooks(response.data);
+        setBooks(response.data.results);
       } catch (error) {
         console.error("Error fetching books:", error);
       }
@@ -24,17 +29,56 @@ const BookList = () => {
   return (
     <div className={css.bookList}>
       <h1>Recommended</h1>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+
+      {Array.isArray(books) && books.length > 0 && (
+        <>
+          <div>
+            <button ref={prevRef} className={css.customPrevBtn}>
+              <svg width={16} height={16}>
+                <use href="/public/sprite.svg#swiper-arrow"></use>
+              </svg>
+            </button>
+            <button ref={nextRef} className={css.customNextBtn}>
+              <svg width={16} height={16}>
+                <use href="/public/sprite.svg#swiper-arrow"></use>
+              </svg>
+            </button>
+          </div>
+
+          <Swiper
+            navigation={{
+              prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            }}
+            modules={[Navigation]}
+            slidesPerView={2}
+            spaceBetween={20}
+            onBeforeInit={(swiper) => {
+              if (
+                typeof swiper.params.navigation !== "boolean" &&
+                swiper.params.navigation
+              ) {
+                swiper.params.navigation.prevEl = prevRef.current;
+                swiper.params.navigation.nextEl = nextRef.current;
+              }
+            }}
+          >
+            {books.map((book) => (
+              <SwiperSlide key={book._id}>
+                <div className={css.contentWrapper}>
+                  <img
+                    className={css.contentImg}
+                    src={book.imageUrl}
+                    alt={book.title}
+                  />
+                  <h3 className={css.contentTitle}>{book.title}</h3>
+                  <p className={css.authorPh}>{book.author}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
     </div>
   );
 };
